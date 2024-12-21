76a20d6b1eec8f8d960bd1edbf1e2f5ab3015fdf
Shahani Vicio <vicioshahani2222@gmail.com>
2024-12-21 23:55:56 +0800

    Update TokenContractDeployment.sol

--- TokenContractDeployment.sol
+++ TokenContractDeployment.sol
@@ -4,14 +4,13 @@ pragma solidity ^0.8.0;
 // Importing OpenZeppelin contracts
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 // Goldi1ocks Token (GLD1)
 // Only contract owner should be able to mint tokens
 // Any user can transfer tokens
 // Any user can burn tokens
 
-contract Goldi1ocks is ERC20, ERC20Burnable, Ownable {
+contract Goldi1ocks is ERC20, Ownable {
 
     constructor(address initialOwner) 
         ERC20("Goldi1ocks", "GLD1") 
